28c546482d9c08c12b056298bee2173e971aafbd
Jefferin Joseph <j.joseph@fugro.com>
2020-08-12 20:02:12 -0400

    Code formatting / optimization

--- src/index.ts
+++ src/index.ts
@@ -1595,22 +1595,25 @@ class HexCodeToColorName {
     ];
 
     name(color: string) {
-        color = color.toUpperCase();
-        if (color.length < 3 || color.length > 7)
+        if (color.length < 3 || color.length > 7) {
             return ["#000000", `Invalid Color: ${color}`, false];
-        if (color.length % 3 == 0)
-            color = `#${color}`;
-        if (color.length == 4)
-            color = `#${color.substr(1, 1)}${color.substr(1, 1)}${color.substr(2, 1)}${color.substr(2, 1)}${color.substr(3, 1)}${color.substr(3, 1)}`;
+        }
 
-        let rgb = this.rgb(color);
-        let r = rgb[0], g = rgb[1], b = rgb[2];
-        let hsl = this.hsl(color);
-        let h = hsl[0], s = hsl[1], l = hsl[2];
+        color = color.toUpperCase();
+        if (color.length % 3 == 0) {
+            color = `#${color}`;
+        }
+        if (color.length == 4) {
+            color = `#${color.substr(1, 1)}${color.substr(1, 1)}${color.substr(2, 1)}${color.substr(2, 1)}${color.substr(3, 1)}${color.substr(3, 1)}`;
+        }
+
+        const [r, g, b] = this.rgb(color);
+        const [h, s, l] = this.hsl(color);
         let ndf1 = 0;
         let ndf2 = 0;
         let ndf = 0;
-        let cl = -1, df = -1;
+        let cl = -1;
+        let df = -1;
 
         for (var i = 0; i < this.names.length; i++) {
             if (color == `#${this.names[i][0]}`) {
@@ -1630,9 +1633,7 @@ class HexCodeToColorName {
     }
 
     private hsl(color: string) {
-        const rgb = [parseInt(`0x${color.substring(1, 3)}`) / 255, parseInt(`0x${color.substring(3, 5)}`) / 255, parseInt(`0x${color.substring(5, 7)}`) / 255];
-        const [r, g, b] = rgb;
-
+        const [r, g, b] = [parseInt(`0x${color.substring(1, 3)}`) / 255, parseInt(`0x${color.substring(3, 5)}`) / 255, parseInt(`0x${color.substring(5, 7)}`) / 255];
         const min = Math.min(r, Math.min(g, b));
         const max = Math.max(r, Math.max(g, b));
         const delta = max - min;
